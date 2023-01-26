8f1209f05cce419ae58e8431f135b0b55eab7b33
Washinfron Fernandes <wmfmg@hotmail.com>
2023-01-25 23:36:43 -0300

    Retirando espaços

--- PositivoNegativoParImpar/Program.cs
+++ PositivoNegativoParImpar/Program.cs
@@ -12,25 +12,15 @@ namespace PositivoNegativoParImpar
             numero = int.Parse(Console.ReadLine());
 
             if (numero % 2 == 0 && numero > 0)
-            {
                 Console.WriteLine("Número Par / Positivo");
-            }
             else if (numero % 2 == 0 && numero < 0)
-            {
                 Console.WriteLine("Número Par / Negativo ");
-            }
-            else if (numero == 0) 
-            {
-                Console.WriteLine("Zero é NEUTRO");            
-            }
-            else if (numero % 2 == 1 && numero > 0) 
-            {
-                Console.WriteLine("Número Ímpar / Positivo ");            
-            }
+            else if (numero == 0)
+                Console.WriteLine("Zero é NEUTRO");
+            else if (numero % 2 == 1 && numero > 0)
+                Console.WriteLine("Número Ímpar / Positivo ");
             else if (numero % 2 == -1 && numero < 0)
-            {
                 Console.WriteLine("Número Ímpar / Negativo ");
-            }
 
             Console.ReadKey();
         }
